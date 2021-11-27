a94e05a523ab660ce01661fe74500d95c2121998
Daniele Marques <danirbd13@hotmail.com>
2021-11-26 23:34:21 -0300

    subindo alterações

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Search } from "./components/Atividade/Search";
+// import { Requisicao } from "./components/Revisando/Req";
+// import { Nome } from "./components/Nome";
+// import { Title } from "./components/Title";
+// import { Subtitle } from "./components/Subtitle";
+// import { UpperCase } from "./components/Uppercase";
 
+// import { CardReq } from "./components/Req";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Search></Search>
+        {/* <Requisicao></Requisicao> */}
+        {/* <Nome />
+        <Title text="Gui" />
+        <Title text="Pan" />
+        <Title text="Pluto" />
+        <Title text="Frida" />
+        <Subtitle>Sim</Subtitle>
+        <Subtitle>Não</Subtitle>
+        <Subtitle>Talvez</Subtitle>
+        <UpperCase recebido="lol" />
+        <CardReq /> */}
       </header>
     </div>
   );
